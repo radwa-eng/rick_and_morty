6f9f37ef5599c7ed263232724d7c03526167a8aa
radwa-eng <radwa.31057428@ics.tanta.edu.eg>
2023-12-11 19:35:25 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_breacking/app_router.dart';
+
+void main() {
+  runApp(BreakingBadApp(
+    appRouter: AppRouter(),
+  ));
+}
+
+class BreakingBadApp extends StatelessWidget {
+  const BreakingBadApp({super.key, required this.appRouter});
+  final AppRouter appRouter;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      onGenerateRoute: appRouter.generatRoute,
+    );
+  }
+}
